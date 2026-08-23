8c4dcedfac72ad1be05f04e0d7ad89931171618d
Warren Halderman <halderman.warren@gmail.con>
2026-08-24 07:17:55 +0900

    add hyprcursor env vars

--- hypr/.config/hypr/envs.lua
+++ hypr/.config/hypr/envs.lua
@@ -1,3 +1,6 @@
+hl.env("HYPRCURSOR_THEME", "Future-Cyan-Hyprcursor_Theme")
+hl.env("HYPRCURSOR_SIZE", "30")
+
 hl.env("NVD_BACKEND", "direct")
 hl.env("LIBVA_DRIVER_NAME", "nvidia")
 hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
